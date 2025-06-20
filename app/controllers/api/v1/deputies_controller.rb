db6b485e824ebe6627c6224f2e18480149cc1c75
kamila <camargo.kmila@gmail.com>
2025-06-19 23:36:45 -0300

    adiciona endpoint para total_spending

--- app/controllers/api/v1/deputies_controller.rb
+++ app/controllers/api/v1/deputies_controller.rb
@@ -5,6 +5,17 @@ module Api
         deputies = Deputy.all
         render json: DeputySerializer.new(deputies).serializable_hash
       end
+
+      def total_spending
+        deputy = Deputy.find_by(id: params[:id])
+
+        if deputy
+          total = deputy.costs.sum(:vlrLiquido)
+          render json: { deputy_id: deputy.id, total_spending: total.to_f }
+        else
+          render json: { error: 'Deputy not found' }, status: :not_found
+        end
+      end
     end
   end
 end
