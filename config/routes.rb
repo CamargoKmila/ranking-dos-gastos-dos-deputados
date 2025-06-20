1cc945934627352cb34455262eb4932bed9cda41
kamila <camargo.kmila@gmail.com>
2025-06-19 23:37:03 -0300

    adiciona rota

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :deputies, only: [:index] do
+        get 'total_spending', on: :member
         resources :costs, only: [:index]
       end
     end
