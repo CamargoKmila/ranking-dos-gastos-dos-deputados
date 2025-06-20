16121073860b065797c61c66faa6dd0c55ad9865
kamila <camargo.kmila@gmail.com>
2025-06-20 00:12:42 -0300

    adiciona endpoint para biggest_expense

--- app/controllers/api/v1/deputies_controller.rb
+++ app/controllers/api/v1/deputies_controller.rb
@@ -16,6 +16,20 @@ module Api
           render json: { error: 'Deputy not found' }, status: :not_found
         end
       end
+
+      def biggest_expense
+        deputy = Deputy.find_by(id: params[:id])
+
+        return render json: { error: 'Deputy not found' }, status: :not_found unless deputy
+
+        biggest_expense = deputy.costs.order(vlrLiquido: :desc).first
+
+        if biggest_expense
+          render json: CostSerializer.new(biggest_expense).serializable_hash
+        else
+          render json: { error: 'No expenses found for this deputy' }, status: :not_found
+        end
+      end
     end
   end
 end
