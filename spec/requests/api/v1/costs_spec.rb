673954207c796abc8aa005880524c8faaccc18d7
kamila <camargo.kmila@gmail.com>
2025-06-20 01:22:11 -0300

    refatora e adiciona teste para paginação

--- spec/requests/api/v1/costs_spec.rb
+++ spec/requests/api/v1/costs_spec.rb
@@ -2,22 +2,46 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Costs', type: :request do
   let(:deputy) { create(:deputy) }
-  let!(:costs) { create_list(:cost, 3, deputy: deputy) }
 
   describe 'GET /api/v1/deputies/:deputy_id/costs' do
-    it 'returns all costs of the deputy' do
-      get "/api/v1/deputies/#{deputy.id}/costs"
+    context 'when there are multiple costs' do
+      before do
+        create_list(:cost, 7, deputy: deputy)
+      end
 
-      expect(response).to have_http_status(:ok)
-      json = JSON.parse(response.body, symbolize_names: true)
-      expect(json[:data].length).to eq(3)
-      expect(json[:data].first[:attributes].keys).to include(:txtDescricao, :vlrLiquido)
+      it 'returns the first page with 5 items' do
+        get "/api/v1/deputies/#{deputy.id}/costs", params: { page: 1, per_page: 5 }
+
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(json[:data].size).to eq(5)
+        expect(json[:meta][:current_page]).to eq(1)
+        expect(json[:meta][:total_pages]).to eq(2)
+        expect(json[:meta][:total_count]).to eq(7)
+        expect(json[:data].first[:attributes].keys).to include(:txtDescricao, :vlrLiquido)
+      end
+
+      it 'returns the second page with 2 items' do
+        get "/api/v1/deputies/#{deputy.id}/costs", params: { page: 2, per_page: 5 }
+
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(json[:data].size).to eq(2)
+        expect(json[:meta][:current_page]).to eq(2)
+        expect(json[:meta][:total_pages]).to eq(2)
+        expect(json[:meta][:total_count]).to eq(7)
+      end
     end
 
-    it 'returns 404 if deputy not found' do
-      get "/api/v1/deputies/999999/costs"
-      expect(response).to have_http_status(:not_found)
-      expect(JSON.parse(response.body)['error']).to eq('Deputy not found')
+    context 'when deputy does not exist' do
+      it 'returns 404' do
+        get "/api/v1/deputies/999999/costs"
+
+        expect(response).to have_http_status(:not_found)
+        expect(JSON.parse(response.body)['error']).to eq('Deputy not found')
+      end
     end
   end
 end
