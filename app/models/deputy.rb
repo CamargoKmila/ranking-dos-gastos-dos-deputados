8276c34bc768c2187f08f3c5cb590fd6ef711731
kamila <camargo.kmila@gmail.com>
2025-06-24 15:34:48 -0300

    adiciona foto para os deputados

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -4,4 +4,8 @@ class Deputy < ApplicationRecord
   validates :txNomeParlamentar, presence: true
   validates :sgPartido, presence: true
   validates :sgUF, presence: true
+
+  def photo_url
+    "https://www.camara.leg.br/internet/deputado/bandep/#{ideCadastro}.jpg"
+  end
 end
